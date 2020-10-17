5fb5f4707344ac19df97bc0e7fa0640a088c8fb8
FeryPan <543563458@qq.com>
2020-10-17 14:38:46 +0800

    finish use action to generate subscribe

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const fs = require('fs')
+const Gists = require('gists')
+const { VMESS_LINK, GH_USERNAME, PASSWORD, GIST_ID } = process.env
 
-fs.readFile('./url.txt', (err, data) => {
-  if (err) throw err;
-  const subscribeContent = data.toString('base64')
-  fs.writeFile('./urlEn.txt', subscribeContent, (err) => {
-    if (err) throw err;
-  })
+const gists = new Gists({
+  username: GH_USERNAME,
+  password: PASSWORD
 })
+
+const subscribeContent = VMESS_LINK.toString('base64')
+
+gists.edit(GIST_ID, {
+  files: {
+    'v2ray_subscribe.txt': {
+      content: subscribeContent
+    }
+
+  }
+})
+  .then(() => console.log('😊 successfully generate subscribe link!'))
+  .catch(err => console.error(err))
