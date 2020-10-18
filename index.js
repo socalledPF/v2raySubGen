66e0a32af50294848ed465ae73c8d168dbf06a61
FeryPan <543563458@qq.com>
2020-10-18 20:15:11 +0800

    test

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const Gists = require('gists')
-const { VMESS_LINK, GH_USERNAME, PASSWORD, GIST_ID } = process.env
+const { GH_USERNAME, PASSWORD, SUBSCRIBE_GIST_ID, VMESS_GIST_ID } = process.env
 
 const gists = new Gists({
   username: GH_USERNAME,
   password: PASSWORD
 })
 
-const vmessBuf = Buffer.from(VMESS_LINK)
-const subscribeContent = vmessBuf.toString('base64')
+async function getVmess() {
+  const { files: { 'v2ray_vmess.txt': { content } } } = await gists.get(VMESS_GIST_ID)
+  console.log(content)
+  return content
+}
 
-gists.edit(GIST_ID, {
-  files: {
-    'v2ray_subscribe.txt': {
-      content: subscribeContent
+async function writeSubscribeContent() {
+  const subscribeContent = await getVmess()
+  await gists.edit(SUBSCRIBE_GIST_ID, {
+    files: {
+      'v2ray_subscribe.txt': {
+        content: subscribeContent
+      }
+  
     }
+  })
+} 
 
+async function run() {
+  try {
+    await writeSubscribeContent()
+    console.log('😊 successfully generate subscribe link!')
   }
-})
-  .then(() => console.log('😊 successfully generate subscribe link!'))
-  .catch(err => console.error(err))
+  catch (err) {
+    console.log(err)
+  }
+}
+
+run()
