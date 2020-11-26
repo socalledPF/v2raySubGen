204a19d3a583ecd60d543386758e82cabbc431ad
FeryPan <543563458@qq.com>
2020-11-26 23:43:05 +0800

    fix

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const Gists = require('gists')
-const { GH_USERNAME, PASSWORD, SUBSCRIBE_GIST_ID, VMESS_GIST_ID } = process.env
 
-const gists = new Gists({
+import axios from 'axios'
+const { GH_TOKEN, GH_USERNAME, SUBSCRIBE_GIST_ID, VMESS_GIST_ID } = process.env
+
+const auth = {
   username: GH_USERNAME,
-  password: PASSWORD
+  password: GH_TOKEN
+}
+
+const ghRequest = axios.create({
+  auth,
+  baseURL: 'https://api.github.com'
 })
 
 async function getVmess() {
-  const { body: { files: { 'v2ray_vmess.txt': { content } } } }= await gists.get(VMESS_GIST_ID)
+  const { data: { files: { 'v2ray_vmess.txt': { content } } } }= await ghRequest.get(`/gists/${VMESS_GIST_ID}`)
   return content
+}
+
+async function editVemss(files, gistId) {
+  await ghRequest.patch(`/gists/${gistId}`, {
+    "gist_id": gistId,
+    files
+  })
 }
 
 async function writeSubscribeContent() {
   const vmessContent = await getVmess()
   const subscribeContent = Buffer.from(vmessContent).toString('base64')
-  await gists.edit(SUBSCRIBE_GIST_ID, {
-    files: {
-      'v2ray_subscribe.txt': {
-        content: subscribeContent
-      }
-  
+  const files = {
+    'v2ray_subscribe.txt': {
+      content: subscribeContent
     }
-  })
+  }
+  await editVemss(files, SUBSCRIBE_GIST_ID)
 } 
 
 async function run() {
